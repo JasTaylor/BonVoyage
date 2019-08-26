8c383663839f81ab90181a8960728fc7c00f4d42
JasTaylor <jjoitaylor@gmail.com>
2019-08-26 15:00:37 -0400

    Added Links to Nav Bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,7 @@ const authenticatedOptions = (
   <Fragment>
     <Nav.Link href="#change-password">Change Password</Nav.Link>
     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
+    <Nav.Link href="#createplace">Create New Place</Nav.Link>
   </Fragment>
 )
 
@@ -18,7 +19,8 @@ const unauthenticatedOptions = (
 
 const alwaysOptions = (
   <Fragment>
-    <Nav.Link to="/">Home</Nav.Link>
+    <Nav.Link href="/">Home</Nav.Link>
+    <Nav.Link href="#places">See All Places</Nav.Link>
   </Fragment>
 )
 
